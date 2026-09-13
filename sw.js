93729dd57e5e8d703a63b6876992bb6fa2e52122
JinwooSon1988 <sonjinwoo1988@gmail.com>
2026-09-13 14:29:36 +0900

    QA: bump service worker cache for v5.0 Stable

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='tfd-v50-rc1';
+const CACHE='tfd-v50-stable-1';
 const CORE=['./','./index.html','./offline.html','./manifest.webmanifest','./search-live.js','./profile-context.js',
 './data/plants.json','./data/korean_retail_name_map.json','./profile/','./meal/','./weekly/','./settings/'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
  if(e.request.method!=='GET')return;
  if(e.request.mode==='navigate'){
